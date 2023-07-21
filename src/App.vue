838a5c96963889906ad6a6c7dc8553f27750ecc7
Jay Sm <jaysmailg14@gmail.com>
2023-07-21 16:15:19 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app {
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Main View</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h1 {
+    margin: 0px;
+  }
+</style>
